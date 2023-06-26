bee41efbeaa065a53438b5dceae635a3af468ab5
Mohit Sigchi <50646753+Sigchi98@users.noreply.github.com>
2023-06-26 10:22:12 +0530

    Create Question_No_3.cpp
    Solution to question no 3 of dsa assignment #2 pwskills ppt program

---
+++ Question_No_3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        int maxLength = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int countPlus1 = 0;
+            int countMinus1 = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (nums[j] == nums[i])
+                    countPlus1++;
+                else if (nums[j] == nums[i] - 1)
+                    countMinus1++;
+        }
+
+        if (countPlus1 > 0 && countMinus1 > 0)
+            maxLength = std::max(maxLength, countPlus1 + countMinus1);
+    }
+
+    return maxLength;
+    }
+};
